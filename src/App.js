515d327d9c2b520faf6a66c7aec69c7c5601696c
unknown <me@here.com>
2022-11-15 20:24:29 +0300

    degisti bazi yaraklar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
-
+import logo from "./logo.svg"
+import styles from './App.module.css'
 
 function App() {
 
   return (
-    <div className="App">
+    <div className={styles.App}>
       <h3>{process.env.NODE_ENV}</h3>
       <p>
         {process.env.REACT_APP_API_URL}
